217c85e29af5ba55065b5e38660f8999868dcb7e
moses000 <imoleayomoses0@gmail.com>
2022-02-05 01:33:35 +0100

    adding style

--- server/controllers/header.php
+++ server/controllers/header.php
@@ -2,25 +2,39 @@
 <html>
     <head>
         <title>glambydebbie</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Glambydebbie Makeover">
+        <meta name="keywords" content="Beauty, Fashion, Makeover">
+        <meta name="author" content="Imoleayo">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../css/style.css" type="text/css" rel="stylesheet">
-        <meta charset=""/>
-        <meta content=""/>
-        <meta description="" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
         <header>
             <div class="topLogo">
-                <img scr="" alt="logo">
+                <img class="img-responsive center glow" scr="../../client/img/logo.png" alt="logo">
             </div>
             <div class="navigation">
-                <nav>
-                    <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Booking</a></li>
-                        <li><a href="">Our touch</a></li>
-                        <li><a href="">Register</a></li>
+            <nav class="navbar navbar-inverse navbar-custom">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Glambydebbie</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Booking</a></li>
+                        <li><a href="#">Our Touch</a></li>
                     </ul>
-                </nav>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
+            </nav>
             </div>
             <div class="bgHeader">
                 <div class="address">
