28ef826f5fc51308dd4c48e5d17756b5939be186
moses000 <imoleayomoses0@gmail.com>
2022-02-06 12:43:58 +0100

    add email

--- server/booking.php
+++ server/booking.php
@@ -27,6 +27,51 @@
             $stmt->bind_param("ssssss", $firstname, $lastname, $address, $phone, $booking, $date_booked);
 
             if($stmt->execute()){
+            //send email
+                $to = "imoleayomoses0@gmail.com";
+                $subject = "GLAMBYDEBBIE BOOKING";
+
+                $message = "
+                ?>
+                <html>
+                <head>
+                <title>Booking</title>
+                </head>
+                <body>
+                <p>Hurray!!!, you can successfully make your booking with glambydebbie. we will contact you shortly</p>
+                <table>
+                <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Description</th>
+                <th>Date booked</th>
+                </tr>
+                <tr>
+                <td><?php echo $firstname; ?></td>
+                <td><?php echo $lastname; ?></td>
+                <td><?php echo $address; ?></td>
+                <td><?php echo $phone; ?></td>
+                <td><?php echo $booking; ?></td>
+                <td><?php echo $date_booked; ?></td>
+                </tr>
+                </table>
+                </body>
+                </html>
+                ";
+<?php
+                // Always set content-type when sending HTML email
+                $headers = "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+                // More headers
+                $headers .= 'From: imoleayothestembelle@gmail.com' . "\r\n";
+                $headers .= 'Cc: amojisola06@gmail.com' . "\r\n";
+
+                mail($to,$subject,$message,$headers);
+            
+
              header("Location: result.php?result=sucess");
             }else{
                 header("Location: result.php?result=failure");
